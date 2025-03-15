628bbbed6f4284063208f87e62f83d01259828fa
parker <parkerg34@gmail.com>
2025-03-15 05:47:54 -0400

    basic MVC architecture

---
+++ backend/services-controller.py
@@ -0,0 +1,11 @@
+
+
+SERVICES = [
+    'voice-analysis',
+    'handwriting-analysis'
+]
+
+class ServicesController:
+    def __init__(self):
+        self.services = SERVICES
+         
---
+++ backend/services/handwriting-service.py
@@ -0,0 +1,6 @@
+import json
+
+
+class HandwritingService:
+    def __init__(self, google_api_key:str):
+        self.
